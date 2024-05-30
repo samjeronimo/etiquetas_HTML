300471010b264c42ae0537479b63c6c8475a7059
SamuelSarazua <sesarazua@scl.edu.gt>
2024-05-30 12:43:26 -0600

    Formulario de Sarazua

--- section3/section3.js
+++ section3/section3.js
@@ -1,5 +1,52 @@
 let dom_seccion3 = document.querySelector(".section3")
 
 dom_seccion3.innerHTML = `
-    hola soy seccion 3
+<h1>Formulario</h1>
+<table border="1">
+    <tr>
+        <td>form</td>
+        <td>Define un formulario.</td>
+    </tr>
+    <tr>
+        <td>fieldset</td>
+        <td>Permite organizar en grupos los campos de un formulario.</td>
+    </tr>    
+    <tr>
+        <td>legend</td>
+        <td>Representa el título de un "fieldset".</td>
+    </tr>   
+     <tr>
+        <td>label</td>
+        <td>Representa el título de un elemento de control de un formulario.</td>
+    </tr>   
+     <tr>
+        <td>input</td>
+        <td>Se usa para crear controles interactivos que reciben datos del usuario.</td>
+    </tr>   
+     <tr>
+        <td>button</td>
+        <td>Representa un botón.</td>
+    </tr>    
+    <tr>
+        <td>select</td>
+        <td>Representa un elemento de control que permite la selección entre un conjunto de opciones "option".</td>
+    </tr>
+    <tr>
+        <td>option</td>
+        <td>Representa una opción en un elemento "select" o "datalist".</td>
+    </tr>
+    <tr>
+        <td>optgroup</td>
+        <td>Representa un conjunto de opciones, agrupadas lógicamente dentro de un "select".</td>
+    </tr>
+    <tr>
+        <td>datalist</td>
+        <td>Se utiliza junto con la etiqueta "input" para proporcionar una lista de opciones "option" que ayudan a los usuarios a seleccionar un valor mientras escriben en un campo de entrada "input".</td>
+    </tr>
+    <tr>
+        <td>textarea</td>
+        <td>Representa una caja de edición de texto en varias líneas.
+        </td>
+    </tr>
+</table>
 `;
