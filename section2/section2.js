dff67a19cc1bb73409c125995f2834a294472933
bemorales <bemorales@scl.edu.gt>
2024-05-30 12:07:59 -0600

    texto_Brian

--- section2/section2.js
+++ section2/section2.js
@@ -1,5 +1,48 @@
 let dom_seccion2 = document.querySelector(".section2")
 
 dom_seccion2.innerHTML = `
-    hola soy seccion2
+<h1 class="texto">texto</h1>
+<table border="1">
+    <tr>
+        <td>html</td>
+        <td>Representa el documento HTML completo.</td>
+    </tr>
+    <tr>
+        <td>head</td>
+        <td>Contiene metadatos sobre el documento, como su título, enlaces a scripts y hojas de estilo.</td>
+    </tr>
+    <tr>
+        <td>title</td>
+        <td>Define el título del documento que se muestra en la barra de título del navegador o en la pestaña.</td>
+    </tr>
+    <tr>
+        <td>meta</td>
+        <td>Define metadatos como la descripción, las palabras clave y el autor del documento.</td>
+    </tr>
+    <tr>
+        <td>link</td>
+        <td>Se usa para enlazar documentos externos como hojas de estilo CSS.</td>
+    </tr>
+    <tr>
+        <td>body</td>
+        <td>Contiene el contenido principal del documento HTML.</td>
+    </tr>
+    <tr>
+        <td>h1 a h6</td>
+        <td>Definen encabezados, siendo h1 el más importante y h6 el menos importante.</td>
+    </tr>
+    <tr>
+        <td>p</td>
+        <td>Define un párrafo de texto.</td>
+    </tr>
+    <tr>
+        <td>a</td>
+        <td>Define un enlace hipertexto.</td>
+    </tr>
+    <tr>
+        <td>img</td>
+        <td>Inserta una imagen en el documento.</td>
+    </tr>
+</table>
 `
+;
